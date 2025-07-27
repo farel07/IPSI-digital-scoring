ae44ddb3eb0dc8e82a6627ee54b605a7a072501f
Ogik29 <dimasoki1234@gmail.com>
2025-07-27 21:15:08 +0700

    first init

---
+++ database/migrations/2025_07_27_140021_create_matches_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->dateTime('date');
+            $table->string('status');
+            $table->foreignId('arena_id')->constrained('arena')->onDelete('cascade');
+            $table->foreignId('bracket_id')->constrained('bracket')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
