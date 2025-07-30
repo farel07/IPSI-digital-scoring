8b22bc0ac7a8baec7416e0cc594a38c9142c6ade
Farrel Aqeel Danendra <farrqeel@gmail.com>
2025-07-30 22:12:58 +0700

    membuat event kirim peringatan

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -13,3 +13,19 @@ function kirimBinaan() {
                 alert("Berhasil!");
             });
         }
+
+function kirimPeringatan() {
+    // alert("Fitur ini belum tersedia.");
+            fetch('/kirim-peringatan', {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    isi: "ini peringatan dari dewan"
+                })
+            }).then(res => res.json()).then(data => {
+                alert("Berhasil!");
+            });
+        }
